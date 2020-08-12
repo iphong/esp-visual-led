762c025cda6d39ec8cacb104c13087fee751e201
Phong Vu <phong.vu@me.com>
2020-08-12 07:03:42 +0700

    Refine code

--- include/show.h
+++ include/show.h
@@ -1,42 +1,19 @@
 #include "Arduino.h"
 #include "LittleFS.h"
 #include "FastLED.h"
-#include "Ticker.h"
-#include "EspRC.h"
 
 class VisualPOI {
 protected:
-    Ticker timeline;
     CRGB _leds[100];
     u8 _led_nums;
 
-    u8 r = 0;
-    u8 g = 0;
-    u8 b = 0;
-
-    u32 _time;
-
 public:
-    VisualPOI(u8 num) {
+    VisualPOI(u8 num, u8 brightness) {
         _led_nums = num;
     }
     void begin() {
         FastLED.addLeds<WS2812B, DAT_PIN, GRB>(_leds, _led_nums);
         FastLED.setBrightness(255);
-        
-        EspRC.begin(255);
-        EspRC.on("#TIMESYNC#", [this]() {
-            u32 time = (u32)EspRC.getValue().toInt();
-            _time = millis() - time;
-            Serial.printf("SYNCED @ %i \n", time);
-            });
-        // #ifdef MASTER
-        // timeline.attach_ms_scheduled_accurate(1000, [this]() {
-        //     EspRC.send("#TIMESYNC#", millis() - _time);
-        //     });
-        // #endif
-
-        fillColor(0, 0, 0);
     }
     void fillColor(u8 red, u8 green, u8 blue) {
         for (auto i=0; i<_led_nums; i++) {
@@ -53,8 +30,7 @@ public:
         }
         FastLED.show();
     }
-    CRGB hsvToRgb(uint16_t h, uint8_t s, uint8_t v)
-    {
+    static CRGB hsvToRgb(uint16_t h, uint8_t s, uint8_t v) {
         uint8_t f = (h % 60) * 255 / 60;
         uint8_t p = (255 - s) * (uint16_t)v / 255;
         uint8_t q = (255 - f * (uint16_t)s / 255) * (uint16_t)v / 255;
