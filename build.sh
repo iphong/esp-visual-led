b572085821d5c07c2236a01b5b53a477c4295196
Phong Vu <phong.vu@me.com>
2021-07-01 16:33:48 +0700

    Add build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd app
+webpack -p
+
+zip app.zip dist/* css/* img/* app.html remote.html utils.html manifest.json
+cd ..
+
+mkdir -p .build
+rm -Rf .build/*
+mv app/app.zip .build/
+
+pio run -e rx_uart
+pio run -e tx_uart
+
+cp .pio/build/rx_uart/firmware.bin .build/rx.bin
+cp .pio/build/tx_uart/firmware.bin .build/tx.bin
